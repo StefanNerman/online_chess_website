0256998f669900a30ecf9fc45f1d56736f72cc4a
stefannerman <nermanstefan12@gmail.com>
2023-05-06 18:04:03 +0300

    removed broken code

--- online_chess_website/ClientApp/src/pages/social_page/SocialPage.tsx
+++ online_chess_website/ClientApp/src/pages/social_page/SocialPage.tsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react'
 
-
-
 const SocialPage = () => {
 
     const [message, setMessage] = useState('')
@@ -9,22 +7,16 @@ const SocialPage = () => {
     let webSocket: WebSocket
 
     function onClick(){
-        console.log(message, id)
-        webSocket = new WebSocket("wss://localhost:44417", "stringhereok")
-        webSocket.onopen = (event) => {
-            webSocket.send("Here's some text that the server is urgently awaiting!");
-        }
         
     }
 
     function onSend(){
-        webSocket.send("helo from frontend")
+        
     }
 
     return (  
         <div style={{display: 'flex', justifyContent: 'center', marginTop: '5rem'}}>
             <button onClick={onClick} style={{padding: '1rem'}}>connect</button>
-            <br></br>
             <input type='text' onChange={(e) => setMessage(e.target.value)} placeholder='message'></input>
             <input type='text' onChange={(e) => setId(e.target.value)} placeholder='id'></input>
             <button onClick={() => onSend()} style={{padding: '1rem'}}>send text</button>
--- online_chess_website/Program.cs
+++ online_chess_website/Program.cs
@@ -1,4 +1,4 @@
-using System.Net.WebSockets;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,29 +8,12 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-app.UseWebSockets();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
 
 }
-
-app.Use(async (context, next) =>
-{
-    Console.WriteLine(context.ToString);
-    if (context.WebSockets.IsWebSocketRequest)
-    {
-        Console.WriteLine("WEBSOCKET");
-        WebSocket ws = await context.WebSockets.AcceptWebSocketAsync();
-        await next();
-    }
-    else
-    {
-        Console.WriteLine("NOPE");
-        await next();
-    }
-});
 
 app.UseStaticFiles();
 app.UseRouting();
