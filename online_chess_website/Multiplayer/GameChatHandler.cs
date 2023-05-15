1721325701db81a5389341d5c42339551cc744ce
stefannerman <nermanstefan12@gmail.com>
2023-05-15 17:58:20 +0300

    added multiplayer event methods

---
+++ online_chess_website/Multiplayer/GameChatHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace online_chess_website.Multiplayer;
+
+public class GameChatHandler
+{
+    public async Task<string> SendMessage(string messageInfo)
+    {
+        return "";
+    }
+}
---
+++ online_chess_website/Multiplayer/MoveHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace online_chess_website.Multiplayer;
+
+public class MoveHandler
+{
+    public async Task<string> MakeMove(string moveInfo)
+    {
+        return "";
+    }
+}
