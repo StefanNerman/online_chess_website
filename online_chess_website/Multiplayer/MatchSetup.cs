a95ad38b6262a8447204e742de434f8c2a438586
stefannerman <nermanstefan12@gmail.com>
2023-05-23 19:32:32 +0300

    added class for match setup message

--- online_chess_website/Multiplayer/MatchSetup.cs
+++ online_chess_website/Multiplayer/MatchSetup.cs
@@ -4,7 +4,6 @@ public class MatchSetup
 {
     public async Task<string> CreateMatch(string p1Token, string p2Token)
     {
-
         return "Match is found, but i'm not gonna give you any match info cuz fuck you";
     }
 }
---
+++ online_chess_website/Multiplayer/MatchSetupMessage.cs
@@ -0,0 +1,13 @@
+﻿namespace online_chess_website.Multiplayer;
+
+public class MatchSetupMessage
+{
+    public string color {get; set;}
+    public int MATCH_ID { get; set;}
+
+    public MatchSetupMessage(string color, int MATCH_ID)
+    {
+        this.color = color;
+        this.MATCH_ID = MATCH_ID;
+    }
+}
