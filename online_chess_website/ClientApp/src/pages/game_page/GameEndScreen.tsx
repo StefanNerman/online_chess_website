2c599235b5eb3680255b2e291190df08852afcd8
stefannerman <nermanstefan12@gmail.com>
2023-06-23 18:50:33 +0300

    added hide method to endscreen

--- online_chess_website/ClientApp/src/pages/game_page/GameEndScreen.tsx
+++ online_chess_website/ClientApp/src/pages/game_page/GameEndScreen.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { setGameEnd } from './GamePage'
+import ButtonSmall from '../../components/btn_maintheme_small'
 
 interface props {
     duration: string
@@ -13,14 +14,26 @@ const GameEndScreen = ({duration, oldRank, newRank, isVictory}: props) => {
 
     const navigate = useNavigate()
 
-    function close(){
+    function exit(){
         setGameEnd(false)
         navigate('/main-menu')
     }
 
+    function hide(){
+        setGameEnd(false)
+    }
+
     return (  
-        <div className="screenquemode-frame">
-            <button onClick={close}>close</button>
+        <div className="screenquemode-frame gameendscreen-frame">
+            <div className='gameendscreen-content'>
+                <div className='gameendscreen-top'>
+
+                </div>
+                <div className='gameendscreen-bottom'>
+                    <ButtonSmall text={'Main menu'} onClick={() => exit()}/>
+                    <ButtonSmall text={'Chessboard'} onClick={() => hide()}/>
+                </div>
+            </div>
         </div>
     )
 }
