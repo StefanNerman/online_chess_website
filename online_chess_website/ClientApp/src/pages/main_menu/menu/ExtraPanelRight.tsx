0868a7cd3d9068202e264d1446b0aa0912ed7f19
stefannerman <nermanstefan12@gmail.com>
2023-08-21 11:55:13 +0300

    profile picture and name styling complete

--- online_chess_website/ClientApp/src/pages/main_menu/menu/ExtraPanelRight.tsx
+++ online_chess_website/ClientApp/src/pages/main_menu/menu/ExtraPanelRight.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const ExtraPanelRight = () => {
     return (  
         <div className='extrapanel-frame' id='social-panel'>
-            <div className='extrapanel-screen'>
+            <div className='extrapanel-screen' id='social-panel-screen'>
                 <h3>Social</h3>
             </div>
         </div>
