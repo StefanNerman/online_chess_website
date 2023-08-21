bb4eb0f8a17aa2932b18aeec8878044ec6bff3e0
stefannerman <nermanstefan12@gmail.com>
2023-08-21 12:09:13 +0300

    added system to toggle save/cancel buttons

--- online_chess_website/ClientApp/src/pages/main_menu/profile/ProfilePage.tsx
+++ online_chess_website/ClientApp/src/pages/main_menu/profile/ProfilePage.tsx
@@ -1,29 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProfilePicture from './ProfilePicture'
 import RoundEdgeButton from '../../../components/round_edge_button'
 
 
 const ProfilePage = () => {
+
+    const [changeMade, setChangeMade] = useState(false)
+
+    function nameInputAction(e: any){
+        //check if username taken (or check when user clicks save)
+        let input: string = e.target.value
+        if(input === '') {
+            return setChangeMade(false)
+        }
+        setChangeMade(true)
+    }
+
+
     return (  
         <div id='profile-frame'>
             <div className='profile-top-frame'>
                 <ProfilePicture image='image1' id='profile-page-pfp'/>
                 <div className='profile-top-name-container'>
-                    loong_name_12
+                    DoomDoober54
                 </div>
             </div>
             <div className='profile-bottom-frame'>
-                {/*
-                possible settings:
-                change name 
-                change pfp
-                delete profile
-
-                other: 
-                statistics
-                rank
-                */}
-
                 <div className='profile-bottom-stats'>
                     <h4>Player statistics</h4>
                     <div className='proflie-stats-top stats-panel-frame'>
@@ -44,9 +46,8 @@ const ProfilePage = () => {
                 <div className='profile-bottom-settings'>
                     <h4>Profile settings</h4>
                     <div className='profile-change-name-box'>
-                        {/*no need for "change username" button here when change registered save and cancel buttons will show up*/}
                         <label>Change name</label>
-                        <input type='text' placeholder='loong_name_12'></input>
+                        <input type='text' placeholder='DoomDoober54' onChange={(e) => {nameInputAction(e)}}></input>
                     </div>
                     <div className='profile-change-pfp-box'>
                         <label>Change profile picture</label>
@@ -58,10 +59,8 @@ const ProfilePage = () => {
                     </div>
                 </div>
 
-                {/*make it so these buttons only show if you make a change to your profile*/}
-
                 {
-                false &&
+                changeMade &&
                 <div id='page-exit-options-container'>
                     <RoundEdgeButton text='Save' onClick={() => {/*save the changes to database*/}}/>
                     <RoundEdgeButton text='Cancel' onClick={() => {/*navigate to menu or reload profile page*/}}/>
