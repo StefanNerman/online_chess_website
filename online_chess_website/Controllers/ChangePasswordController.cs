d63badb845d706c6e539ca79c04c5eb2f3583fea
stefannerman <nermanstefan12@gmail.com>
2023-08-27 18:00:27 +0300

    added controller for changin password

---
+++ online_chess_website/Controllers/ChangePasswordController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace online_chess_website.Controllers;
+
+[Route("api/change_password")]
+[ApiController]
+public class ChangePasswordController : ControllerBase
+{
+    [HttpGet("{id_and_password}")]
+    public async Task<bool> Get(string id_and_password)
+    {
+        Console.WriteLine("=======> " + id_and_password);
+        string[] words = id_and_password.Split(":");
+
+        int userId = int.Parse(words[0]);
+        string newName = words[1];
+
+        return false;
+    }
+}
