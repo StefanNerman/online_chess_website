9f1eb746d499b8ebbb1ff5ef5e32633b4cf65fe7
stefannerman <nermanstefan12@gmail.com>
2023-08-27 18:13:14 +0300

    serverside code for changing password done

--- online_chess_website/Controllers/ChangePasswordController.cs
+++ online_chess_website/Controllers/ChangePasswordController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GenericClassesLibrary.DataAccess;
+using GenericClassesLibrary.Generic.ChessWebsite.DatabaseTypes;
+using Microsoft.AspNetCore.Mvc;
+using online_chess_website.Data;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -9,14 +12,48 @@ namespace online_chess_website.Controllers;
 public class ChangePasswordController : ControllerBase
 {
     [HttpGet("{id_and_password}")]
-    public async Task<bool> Get(string id_and_password)
+    public async Task<int> Get(string id_and_password)
     {
         Console.WriteLine("=======> " + id_and_password);
         string[] words = id_and_password.Split(":");
 
         int userId = int.Parse(words[0]);
-        string newName = words[1];
+        string newPassword = words[1];
 
-        return false;
+        bool passwordCheck = await CompareOldPassword(userId, newPassword, ConnectionStrings.GetString("defaultString"));
+
+        if (!passwordCheck)
+        {
+            return 0;
+        }
+
+        bool changeStatus = await ChangePassword(userId, newPassword, ConnectionStrings.GetString("defaultString"));
+
+        if (!changeStatus)
+        {
+            return 1;
+        }
+
+        return 2;
+    }
+
+    async Task<bool> ChangePassword(int userId, string newPassword, string connectionString)
+    {
+        MySQL db = new MySQL();
+        string sql = $"UPDATE user_credidentals SET userPassword = '{newPassword}' WHERE userId = {userId}";
+        await db.SaveData(sql, new { }, connectionString);
+        return true;
+    }
+
+    async Task<bool> CompareOldPassword(int userId, string newPassword, string connectionString)
+    {
+        MySQL db = new MySQL();
+        string sql = $"SELECT * FROM user_credidentals WHERE userId = {userId}";
+        List<UserLoginInfo> data = await db.GetData<UserLoginInfo, dynamic>(sql, new { }, connectionString);
+        if (data[0].userPassword == newPassword)
+        {
+            return false;
+        }
+        return true;
     }
 }
