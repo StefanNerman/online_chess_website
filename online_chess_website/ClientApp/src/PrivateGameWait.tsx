3841a9d93d73bc1af18cd02b8c3ffc236d3278c5
stefannerman <nermanstefan12@gmail.com>
2023-12-22 22:52:22 +0200

    changed description for privategamewait

--- online_chess_website/ClientApp/src/PrivateGameWait.tsx
+++ online_chess_website/ClientApp/src/PrivateGameWait.tsx
@@ -12,12 +12,13 @@ const PrivateGameWait = () => {
 
     let gamekey = sessionStorage.getItem('sessionToken')
 
-    //make it so your gamekey (sessiontoken) is visible on this screen
+    //maybe add button to automatically copy the gamekey to users clipboard
 
     return (  
         <div className='screenquemode-frame'>
             <div className='screenquemode-content'>
-                <h2>Your gamekey is: {gamekey}</h2>
+                <h2>Private lobby created</h2>
+                <p>Your gamekey is: {gamekey}</p>
                 <button onClick={handleClick}>Cancel</button>
             </div>
         </div>
